16970da0957b15ed38eea0236dcc38cb2237c9b8
colio1 <randy.colio@colorado.edu>
2022-10-07 08:09:55 -0700

    another try

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -7,16 +7,19 @@
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
 #AESD_ASSIGNMENTS_VERSION = 79357760c01f24d79ec17fcc49ef2d7cf4e7fcb6
-AESD_ASSIGNMENTS_VERSION = 3fcbd252ab59c68bc6eb6f322e20261d651e6956
+#AESD_ASSIGNMENTS_VERSION = 3fcbd252ab59c68bc6eb6f322e20261d651e6956
+#AESD_ASSIGNMENTS_VERSION = 8a8a27ded8b59259dc1953a3ba74e343f6c2667c
+#AESD_ASSIGNMENTS_VERSION = 4a4b17920f85b11213bac1d95670fc427a82b9ed
+AESD_ASSIGNMENTS_VERSION = 'ab1d246a091bb346578e63aae39e71cda7e77c1d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
-AESD_ASSIGNMENTS_SITE = git@github.com:cu-ecen-aeld/assignments-3-and-later-colio1.git
+AESD_ASSIGNMENTS_SITE = 'git@github.com:cu-ecen-aeld/assignments-3-and-later-colio1.git'
 AESD_ASSIGNMENTS_SITE_METHOD = git
 AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
-	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/finder-app CROSS_COMPILE=aarch64-none-linux-gnu- all
 endef
 
 # TODO add your writer, finder and finder-test utilities/scripts to the installation steps below
